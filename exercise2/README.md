fce9b24c60a8e21bd8e171208040c01fec5aee1d
Arthur Holzner <arthur.holz.91@gmail.com>
2019-05-24 10:00:53 +0200

    added readme to exercise 2 + improved comments

---
+++ exercise2/README.md
@@ -0,0 +1,63 @@
+# Exercise 2
+
+Fix the [schema](https://github.com/Speedy1991/graphql_workshop/blob/master/graphql_workshop/settings.py#L51): `exercise2.schema.schema`
+
+## Problem
+At the moment we can't query anything related (e.g. _modules_) on the _ProfessorType_
+
+
+## TODO
+
+- Open [types.py](https://github.com/Speedy1991/graphql_workshop/blob/master/exercise2/schema/types.py)
+- Follow the TODO's in the file
+
+## Questions
+
+- What happens if a `favourite_module` is `None`? Can we enforce the field?
+- How about Lists?
+
+
+## Sample
+
+Query:
+```
+{
+  students {
+    name
+    favouriteModule {
+      id
+      name
+    }
+    modules {
+      id
+      name
+    }
+  }
+}
+```
+
+Result:
+
+```
+{
+  "data": {
+    "students": [
+      {
+        "name": "John",
+        "favouriteModule": {
+          "id": "1",
+          "name": "Math"
+        },
+        "modules": [
+          {
+            "id": "1",
+            "name": "Math"
+          },
+          {
+            "id": "3",
+            "name": "Physics"
+          }
+        ]
+      },
+      ...
+```
--- exercise2/schema/types.py
+++ exercise2/schema/types.py
@@ -5,15 +5,15 @@ class ProfessorType(graphene.ObjectType):
     id = graphene.ID()
     name = graphene.String(required=True)
     age = graphene.Int(required=True)
-    # TODO: add a List of ModuleType's
+    # TODO 1: add a List of modules
     # DOCS: https://docs.graphene-python.org/en/latest/types/list-and-nonnull/
     # HINT: We need a forward declaration because the ModuleType isn't defined at this place graphene.List(lambda: ModuleType)
 
     def resolve_modules(self, info, **kwargs):
         pass
-        # TODO: Resolve the modules
+        # TODO 2: Resolve the modules
         # HINT: self is the current instance (in this case a professor instance)
-        # DJANGO: instance.modules_set.all()
+        # DJANGO: instance.module_set.all()
 
 
 class StudentType(graphene.ObjectType):
@@ -21,18 +21,21 @@ class StudentType(graphene.ObjectType):
     age = graphene.Int(required=True)
     name = graphene.String(required=True)
     semester = graphene.Int(required=True)
+    # TODO 3: add modules
+    # TODO 4: add favourite_subject
 
-    # TODO: Resolve the modules
+    # TODO 5: Resolve the modules
     # HINT: self is the current instance (in this case a student instance)
-    # DJANGO: instance.modules_set.all()
+    # DJANGO: instance.module_set.all()
+    # TODO 6: Resolve favourite_module
 
 
 class ModuleType(graphene.ObjectType):
     id = graphene.ID()
     name = graphene.String(required=True)
-    # TODO: Add a ProfessorType field
+    # TODO 7: Add professor
 
     def resolve_professor(self, info, **kwargs):
         pass
-        # TODO: resolve the professor of the module
+        # TODO 8: resolve the professor
         # HINT: self is the current instance (in this case a module instance)
